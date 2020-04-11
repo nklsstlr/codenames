dae72008a8cdd387a4a865cdf856feade884fa5d
Niklas Seitler <niklas.seitler@gmail.com>
2020-04-11 17:46:14 +0200

    added component interaction

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import { Link } from "react-router-dom";
 
-const Lobby = () => {
-  return <div>lobby</div>;
+const Lobby = ({ location }) => {
+  const { name } = queryString.parse(location.search);
+  return (
+    <div>
+      <h2>HALLO {name}</h2>
+      lobby
+      <ul>
+        <Link to={`/game?name=${name}&room=$1`}>
+          <li>Room 1</li>
+        </Link>
+        <Link to={`/game?name=${name}&room=$2`}>
+          <li>Room 2</li>
+        </Link>
+        <Link to={`/game?name=${name}&room=$3`}>
+          <li>Room 3</li>
+        </Link>
+      </ul>
+    </div>
+  );
 };
 
 export default Lobby;
