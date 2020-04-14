234182d36f3d19af40fa0d0ac9b9e59b297f83dc
Niklas Seitler <niklas.seitler@gmail.com>
2020-04-14 14:52:52 +0200

    auth added

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,25 @@ import Join from "./components/Join/Join";
 import Lobby from "./components/Lobby/Lobby";
 import Game from "./components/Game/Game";
 
+import SignIn from "./components/auth/SignIn/SignIn";
+import SignUp from "./components/auth/SignUp/SignUp";
+import ProfilePage from "./components/auth/ProfilePage/ProfilePage";
+import PasswordReset from "./components/auth/PasswordReset/PasswordReset";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
-  return (
+  const user = null;
+  return user ? (
+    <ProfilePage />
+  ) : (
     <Router>
       <Route path="/" exact component={Join} />
+      {/* 
+      https://blog.logrocket.com/user-authentication-firebase-react-apps/
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signUp" exact component={SignUp} />
+      <Route path="/passwordReset" exact component={PasswordReset} /> */}
+
       <Route path="/lobby" component={Lobby} />
       <Route path="/game" component={Game} />
     </Router>
