a3a82f248762833d78e59782ce1029acfc1ed58f
Niklas Seitler <niklas.seitler@gmail.com>
2020-04-17 23:12:27 +0200

    webpack and babel added
    Co-authored-by: Antony N <antony.nghiem@gmail.com>

--- client/src/components/auth/ProfilePage/ProfilePage.js
+++ client/src/components/auth/ProfilePage/ProfilePage.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
-import { UserContext } from "../../../providers/UserProvider";
 import { auth } from "../../../firebase";
+import { UserContext } from "../../../providers/UserProvider";
 const ProfilePage = () => {
   const user = useContext(UserContext);
+
   const { photoURL, displayName, email } = user;
   return (
     <div className="mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
